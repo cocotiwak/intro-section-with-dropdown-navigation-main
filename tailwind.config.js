138466b3b5ecae7b7553e64b8537c109ebcedcef
danoe <anggoro@gmail.com>
2023-12-27 11:48:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'display': ['Epilogue','serif']
+      },
+      colors:{
+        'Almost-White': 'hsl(0, 0%, 98%)',
+        'Medium-Gray': 'hsl(0, 0%, 41%)',
+        'Almost-Black': 'hsl(0, 0%, 8%)'
+      },
+      fontSize: {
+        basis : ['18px','24px']
+      },
+    },
+  },
+  variants: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
